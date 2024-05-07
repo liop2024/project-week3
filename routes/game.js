5fd1823eef5baba0f3d390cb287e247ebdfce9fe
liop2024 <167613561+liop2024@users.noreply.github.com>
2024-05-07 18:58:42 +0300

    Update game.js

--- routes/game.js
+++ routes/game.js
@@ -1,9 +1,17 @@
-// routes/games.js
-const gamesRouter = require("express").Router(); // Создали роутер
-const { sendAllGames, deleteGame, addGameController } = require("../controllers/games")
-const { getAllGames  } = require("../middlewares/games")
-gamesRouter.post("/games", getAllGames , addGameController)
-gamesRouter.get("/games", getAllGames , sendAllGames)
-gamesRouter.delete("/games/:id", getAllGames , deleteGame);
-
-module.exports = gamesRouter;
+gamesRouter.get("/games", getAllGames, sendAllGames);
+gamesRouter.post(
+  "/games",
+  getAllGames,
+  checkIsTitleInArray,
+  updateGamesArray,
+  updateGamesFile,
+  sendUpdatedGames
+);
+gamesRouter.delete(
+  "/games/:id",
+  getAllGames,
+  findGameById,
+  deleteGame,
+  updateGamesFile,
+  sendUpdatedGames
+);
